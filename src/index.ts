0f793454554b2d2f6e55e9bd1398e3d265107686
José Buschiazzo <104038315+josebuschiazzo2@users.noreply.github.com>
2022-05-30 20:45:47 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
-import "./styles.css";
+/* Jardin
+El jardín infantil necesita un programa para poder asignar cursos a las aulas
+• Se cuentan con tres aulas: Azul, Verde y Amarilla
+• Cada aula cuenta con una capacidad diferente (es decir, un número de
+bancos)
+• La aula Azul tiene 40 bancos, la Verde 35 y la Amarilla 30
+• Dado un número de infantes ingresado por el usuario, el programa deberá
+determinar el aula que minimice la cantidad de bancos vacíos
+• La salida del algoritmo es el color que identifica al aula asignada*/
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+//Empiezo con el main
+console.clear();
+const classroomAzul:number = 40;
+const classroomVerde:number = 35;
+const classroomAmarilla:number = 30;
+
+let numstudents:number=Number(prompt("Ingrese la cantidad de alumnos");
+if (numstudents>106 || numstudents<0 || isNaN(numstudents) || numstudents % 1)
+{
+  alert("Solo numeros enteros entre 0 a 106")
+} else {
+   if (numstudents<31)
+    { 
+    console.log("Utilice el Aula Azul para los " + numstudents +" Alumnos");
+    } else{ 
+       if ((numstudents<61))
+        {
+        console.log("Utilice el Aula Azul para los " + (numstudents/2).toPrecision(2) +" Alumnos");
+        console.log("y Utilice el Aula Verde para los " + (numstudents - (numstudents/2).toPrecision(2)) +" Alumnos");
+        } else {
+           if ((numstudents<66))
+            {
+              console.log("Utilice el Aula Azul para los " + classroomAzul +" Alumnos");
+              console.log("y Utilice el Aula Verde para los " + (numstudents - classroomAzul) +" Alumnos");
+            }
+          } 
+         
+      }
+    
+  }
